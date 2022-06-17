8b1b918a3313eb0246b962c3c55810b3002df185
long6500 <68457680+long6500@users.noreply.github.com>
2022-06-17 16:30:22 +0700

    re animate and joystick control

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,101 +4,37 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private new Rigidbody2D rigidbody;
-    private Vector2 direction = Vector2.down;
-    public float speed = 5f;
 
-    [Header("Input")]
-    public KeyCode inputUp = KeyCode.W;
-    public KeyCode inputDown = KeyCode.S;
-    public KeyCode inputLeft = KeyCode.A;
-    public KeyCode inputRight = KeyCode.D;
-
-    [Header("Sprites")]
-    public AnimatedSpriteRenderer spriteRendererUp;
-    public AnimatedSpriteRenderer spriteRendererDown;
-    public AnimatedSpriteRenderer spriteRendererLeft;
-    public AnimatedSpriteRenderer spriteRendererRight;
-    public AnimatedSpriteRenderer spriteRendererDeath;
-    private AnimatedSpriteRenderer activeSpriteRenderer;
-
-    private void Awake()
-    {
-        rigidbody = GetComponent<Rigidbody2D>();
-        activeSpriteRenderer = spriteRendererDown;
-    }
-
+    public Rigidbody2D rb;
+    private Vector2 movement;
+    public float moveSpeed = 5f;
+    public Animator animator;
+    public MovementJoystick movementJoystick;
     private void Update()
     {
-        if (Input.GetKey(inputUp))
-        {
-            SetDirection(Vector2.up, spriteRendererUp);
-        }
-        else if (Input.GetKey(inputDown))
-        {
-            SetDirection(Vector2.down, spriteRendererDown);
-        }
-        else if (Input.GetKey(inputLeft))
-        {
-            SetDirection(Vector2.left, spriteRendererLeft);
-        }
-        else if (Input.GetKey(inputRight))
-        {
-            SetDirection(Vector2.right, spriteRendererRight);
-        }
-        else
-        {
-            SetDirection(Vector2.zero, activeSpriteRenderer);
-        }
+       // movement.x = movementJoystick.joystickVec.x;
+       // movement.y = movementJoystick.joystickVec.y;
+        animator.SetFloat("Horizontal", movementJoystick.joystickVec.x);
+        animator.SetFloat("Vertical", movementJoystick.joystickVec.y);
+        animator.SetFloat("Speed", movementJoystick.joystickVec.sqrMagnitude);
+       // Debug.Log("phone: " + movementJoystick.joystickVec.x);
+       // Debug.Log("keyboard: " + movement.x);
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
     {
-        Vector2 position = rigidbody.position;
-        Vector2 translation = direction * speed * Time.fixedDeltaTime;
-
-        rigidbody.MovePosition(position + translation);
+        //rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        if (movementJoystick.joystickVec.y != 0)
+        {
+            // rb.velocity = new Vector2(movementJoystick.joystickVec.x * moveSpeed, movementJoystick.joystickVec.y * moveSpeed);
+            rb.MovePosition(rb.position + movementJoystick.joystickVec * moveSpeed * Time.fixedDeltaTime);
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
     }
 
-    private void SetDirection(Vector2 newDirection, AnimatedSpriteRenderer spriteRenderer)
-    {
-        direction = newDirection;
-
-        spriteRendererUp.enabled = spriteRenderer == spriteRendererUp;
-        spriteRendererDown.enabled = spriteRenderer == spriteRendererDown;
-        spriteRendererLeft.enabled = spriteRenderer == spriteRendererLeft;
-        spriteRendererRight.enabled = spriteRenderer == spriteRendererRight;
-
-        activeSpriteRenderer = spriteRenderer;
-        activeSpriteRenderer.idle = direction == Vector2.zero;
-    }
-
-    //private void OnTriggerEnter2D(Collider2D other)
-    //{
-    //    if (other.gameObject.layer == LayerMask.NameToLayer("Explosion"))
-    //    {
-    //        DeathSequence();
-    //    }
-    //}
-
-    //private void DeathSequence()
-    //{
-    //    enabled = false;
-    //    GetComponent<BombController>().enabled = false;
-
-    //    spriteRendererUp.enabled = false;
-    //    spriteRendererDown.enabled = false;
-    //    spriteRendererLeft.enabled = false;
-    //    spriteRendererRight.enabled = false;
-    //    spriteRendererDeath.enabled = true;
-
-    //    Invoke(nameof(OnDeathSequenceEnded), 1.25f);
-    //}
-
-    //private void OnDeathSequenceEnded()
-    //{
-    //    gameObject.SetActive(false);
-    //    FindObjectOfType<GameManager>().CheckWinState();
-    //}
 
 }
